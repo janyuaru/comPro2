b2e373623e6026cd535154c5bf98492df5b7cc0f
jan <139055982+janyuaru@users.noreply.github.com>
2025-02-18 16:53:11 +0700

    Update AliCarCar1.java

--- AliCarCar1.java
+++ AliCarCar1.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-/*
+
 class Car {
     String name;
     int price;
@@ -19,7 +19,8 @@ class Car {
         }
     }
 }
-*/
+
+
 public class AliCarCar1 {
 
     public static void main(String[] args) {
